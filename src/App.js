cee2264b0912b37e1ac6fb422a9c9d45fc1965f7
preetcoder07 <preetgusain84@gmail.com>
2024-05-16 20:03:49 +0530

    ya

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import Center from './components/Center';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 function App() {
   return (
     <>
-    <Navbar/>
+    <Navbar />
+    <Center/>
     </>
   )
 }
---
+++ src/components/Center.js
@@ -0,0 +1,5 @@
+export default function Center(){
+  <div>
+    <h1>hello</h1>
+  </div>
+  }
